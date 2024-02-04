a0ffd159099c6702ac03b5efd6f1a4729b10973a
Vladislav812 <92319406+Vladislav812@users.noreply.github.com>
2024-02-04 10:52:34 +0300

    Delete Vlad/Main.java

--- Vlad/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Singleton singleton1 = Singleton.getSingleton();
-        Singleton singleton2 = Singleton.getSingleton();
-        System.out.println(singleton1 == singleton2);
-    }
-}
