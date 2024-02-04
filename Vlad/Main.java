4b087ca17ad63384711d990284147c93e0c9d401
Vladislav812 <92319406+Vladislav812@users.noreply.github.com>
2024-02-04 10:51:03 +0300

    Add files via upload

---
+++ Vlad/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getSingleton();
+        Singleton singleton2 = Singleton.getSingleton();
+        System.out.println(singleton1 == singleton2);
+    }
+}
